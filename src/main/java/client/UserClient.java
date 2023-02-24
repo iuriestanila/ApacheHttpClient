2d0ebf22bd43ebf603659ace9e10c5c8c90836e5
IurieStanila <stanilaiurie@gmail.com>
2023-02-24 11:12:25 +0200

    Create user implementation 3

--- src/main/java/client/UserClient.java
+++ src/main/java/client/UserClient.java
@@ -11,7 +11,8 @@ import java.util.List;
 
 public class UserClient {
     private static final String POST_USERS_ENDPOINT = "/users";
-    private static final String GET_USERS_ENDPOINT = "/users?";
+    private static final String GET_USERS_ENDPOINT_PARAM = "/users?";
+    private static final String GET_USERS_ENDPOINT = "/users";
     private final ObjectMapper objectMapper;
 
     public UserClient() {
@@ -25,9 +26,19 @@ public class UserClient {
         return httpResponse.getStatusLine().getStatusCode();
     }
 
+    public ResponseEntity<List<User>> getUsers() {
+        final HttpResponse httpResponse = Client.doGet(GET_USERS_ENDPOINT);
+        return handleResponse(httpResponse);
+    }
+
+
+    public ResponseEntity<List<User>> getUsersParam(String key, String value) {
+        final HttpResponse httpResponse = Client.doGet(GET_USERS_ENDPOINT_PARAM, key, value);
+        return handleResponse(httpResponse);
+    }
+
     @SneakyThrows
-    public ResponseEntity<List<User>> getUsers(String key, String value) {
-        final HttpResponse httpResponse = Client.doGet(GET_USERS_ENDPOINT, key, value);
+    private ResponseEntity<List<User>> handleResponse(HttpResponse httpResponse){
         ResponseEntity<List<User>> response = new ResponseEntity<>();
         response.setStatusCode(httpResponse.getStatusLine().getStatusCode());
         List<User> appUsers = new ObjectMapper().readValue(httpResponse.getEntity().getContent(), new TypeReference<>() {
